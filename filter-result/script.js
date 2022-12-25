024dbe67c38ebe3454713684eba87d8cfe27ff7c
sukhepadda <paddasukhwantsingh@gmail.com>
2022-12-25 22:52:01 +0530

    almost done
    almost done but i can try if i can add little bit more
    
    almost done!

--- filter-result/script.js
+++ filter-result/script.js
@@ -1,1 +1,17 @@
-console.log("hello world!");
+function toggleFilters(){
+    const filters = document.getElementById
+    ("filters");
+    const arrow = document.getElementById
+    ("filtersButtonArrow");
+    
+    if (filters.classList.contains("noDisplay")){
+      filters.classList.remove("noDisplay");
+      arrow.classList.add('arrowUp');
+      arrow.classList.remove('arrowDown');
+    }
+    else{
+      filters.classList.add("noDisplay");
+      arrow.classList.remove('arrowUp');
+      arrow.classList.add('arrowDown');
+    }
+  }
