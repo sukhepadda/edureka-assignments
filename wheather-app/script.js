138dbb6f85115ba68b2737b65923a354ac35b2a6
sukhepadda <paddasukhwantsingh@gmail.com>
2023-01-24 18:44:41 +0530

    added icon

--- wheather-app/script.js
+++ wheather-app/script.js
@@ -5,6 +5,7 @@ let temp = document.querySelector("#temp");
 let skyData = document.querySelector("#skyData");
 let humidity = document.querySelector("#humidity");
 let windSpeed = document.querySelector("#windSpeed");
+let imgIcon = document.querySelector("#imgIcon");
 
 const API_KEY = "18d886945f196f51280ce1ebc369397e";
 
@@ -21,9 +22,11 @@ btnId.addEventListener("click", () => {
       let fetchSky = data.weather[0].description;
       let fetchHumidity = data.main.humidity;
       let fetchSpeed = data.wind.speed;
+      let fetchIcon = data.weather[0].icon;
       console.log(data);
       locationHead.innerText = `Wheather in ${fetchCity}`;
       temp.innerHTML = `${fetchTemp}&#176; C`;
+      imgIcon.src = `http://openweathermap.org/img/wn/${fetchIcon}@2x.png`;
       skyData.innerHTML = `${fetchSky}`;
       humidity.innerHTML = `Humidity: ${fetchHumidity}%`;
       windSpeed.innerHTML = `Wind Speed: ${fetchSpeed}km/h`;
